2d70c00c532d3fc44cd3cf0876994cf7fa523e97
Mohammad Nasirifar <m.nasirifar@live.com>
2016-05-18 13:36:32 +0430

    Removed unnecessary import path from protoc command

--- proto/generate.go
+++ proto/generate.go
@@ -1,4 +1,4 @@
 package proto
 
-//go:generate ${PROTOC} -I${GOPATH}/src/github.com/google/protobuf/src -I${GOPATH}/src/github.com -I/usr/local/include -I. -I${GOPATH}/src -I${GOPATH}/src/github.com/gengo/grpc-gateway/third_party/googleapis --go_out=Mgoogle/api/annotations.proto=github.com/gengo/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:. common.proto public.proto internal.proto
-//go:generate ${PROTOC} -I${GOPATH}/src/github.com/google/protobuf/src -I${GOPATH}/src/github.com -I/usr/local/include -I. -I${GOPATH}/src -I${GOPATH}/src/github.com/gengo/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true:. common.proto public.proto internal.proto
+//go:generate ${PROTOC} -I${GOPATH}/src/github.com/google/protobuf/src -I/usr/local/include -I. -I${GOPATH}/src -I${GOPATH}/src/github.com/gengo/grpc-gateway/third_party/googleapis --go_out=Mgoogle/api/annotations.proto=github.com/gengo/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:. common.proto public.proto internal.proto
+//go:generate ${PROTOC} -I${GOPATH}/src/github.com/google/protobuf/src -I/usr/local/include -I. -I${GOPATH}/src -I${GOPATH}/src/github.com/gengo/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true:. common.proto public.proto internal.proto
